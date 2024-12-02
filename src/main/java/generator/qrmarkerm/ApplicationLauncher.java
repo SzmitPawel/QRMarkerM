007f2b6b58ab4f794a2cae5a3a4e69b4c7d69631
szmitp <szmitp@oriax.eu>
2024-12-02 17:54:41 +0100

    added ApplicationLauncher

---
+++ src/main/java/generator/qrmarkerm/ApplicationLauncher.java
@@ -0,0 +1,9 @@
+package generator.qrmarkerm;
+
+import javafx.application.Application;
+
+public class ApplicationLauncher {
+    public static void main(String[] args) {
+        Application.launch(QRMarker.class,args);
+    }
+}
